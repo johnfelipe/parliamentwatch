efd7aa993bc4510905f8bc81fa963074696c7a4d
Stefan Freudenberg <stefan@agaric.com>
2017-04-28 06:38:31 +0200

    Improve database provisioner

--- provisioning/database.sh
+++ provisioning/database.sh
@@ -3,12 +3,16 @@
 if [ -z "$AW_USERNAME" ] || [ -z "$AW_PASSWORD" ]; then
 	echo "In order to fetch the latest database backup your username and"
 	echo "password for http://backups.parliamentwatch.org are required."
-	echo "Please provide those credentials as environment variables"
-	echo "AW_USERNAME and AW_PASSWORD."
+	echo "Please provide those credentials as environment variables:"
+	echo "export AW_USERNAME=... AW_PASSWORD=..."
 	exit 1
 fi
 
-curl -u$AW_USERNAME:$AW_PASSWORD -sS http://backups.parliamentwatch.org/abgeordnetenwatch.de/abgeordnetenwatch.de_latest.sql.gz | gunzip > /tmp/backup.sql
-drush sql-cli < /tmp/backup.sql
-drush cc all
-drush fr-all -y
+echo "This will take a few minutes. Please be patient."
+echo "Downloading latest backup from http://backups.parliamentwatch.org/abgeordnetenwatch.de/abgeordnetenwatch.de_latest.sql.gz..."
+wget --http-user=$AW_USERNAME --http-password=$AW_PASSWORD --progress=bar:force -qO- http://backups.parliamentwatch.org/abgeordnetenwatch.de/abgeordnetenwatch.de_latest.sql.gz | gunzip > /tmp/backup.sql
+echo "Loading data..."
+drush sql-cli -p < /tmp/backup.sql
+drush cc all -p
+echo "Resetting configuration..."
+drush fr-all -y -p
