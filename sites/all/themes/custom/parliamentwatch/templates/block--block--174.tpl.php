258d0183236c17ed15e70c875edad0d33a94e68c
Boris Hekele <hekele@parliamentwatch.org>
2017-05-05 21:15:36 +0200

    AW-3947 add wdr videos

--- sites/all/themes/custom/parliamentwatch/templates/block--block--174.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/block--block--174.tpl.php
@@ -1,3 +1,25 @@
+<?php
+$account_parliament = _pw_get_current_parliament_term()->name;
+$account_name = _pw_get_current_user()->name;
+if($account_parliament != "Nordrhein-Westfalen" || empty($account_name)){
+  return;
+}
+$file = file_get_contents(dirname(__FILE__)."/wdr/candidates.json");
+$json = json_decode($file);
+foreach($json->k as $candidate) {
+  $links = $candidate->l;
+  foreach($links as $link){
+    if(isset($candidate->img) && $link->u == "https://www.abgeordnetenwatch.de/profile/".$account_name){
+      $wdr_id = $candidate->id;
+      $wdr_img_url = "http://".$candidate->img->l;
+      break;
+    }
+  }
+}
+if(!isset($wdr_id)){
+  break;
+}
+?>
 <?php $tag = $block->subject ? 'section' : 'div'; ?>
 <<?php print $tag; ?><?php print $attributes; ?>>
   <div class="block-inner clearfix">
@@ -7,12 +29,11 @@
           <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
         <?php endif; ?>
         <?php print render($title_suffix); ?>
-
-
         <div class="kc_wdr_teaser">
           <!-- http://kandidatencheck-test.wdr.de/kandidatencheck/img/[vn]_[nn]/[id]-L.jpg" alt="">-->
-          <a class="kc_wdr_teaser__image" href="http://kandidatencheck.wdr.de/kandidatencheck/video/682790" target="_blank"><img src="http://kandidatencheck-test.wdr.de/kandidatencheck/img/Carina_Goedecke/682790-L.jpg" alt=""></a>
-          <a class="kc_wdr_teaser__link" href="http://kandidatencheck.wdr.de/kandidatencheck/?kandidat=682790" target="_blank">Zum Profil beim WDR</a>
+          <!-- http://kandidatencheck.wdr.de/kandidatencheck/video/ -->
+          <a class="kc_wdr_teaser__image" href="http://kandidatencheck.wdr.de/kandidatencheck/?kandidat=<?php print $wdr_id; ?>" target="_blank"><img src="<?php print $wdr_img_url; ?>" alt=""></a>
+          <!--a class="kc_wdr_teaser__link" href="http://kandidatencheck.wdr.de/kandidatencheck/?kandidat=<?php print $wdr_id; ?>" target="_blank">Zum Profil beim WDR</a-->
         </div>
     </div>
   </div>
