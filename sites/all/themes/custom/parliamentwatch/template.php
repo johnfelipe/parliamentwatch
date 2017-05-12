148d64fe5e663767a736a6e6b1d38c37bacba37c
Stefan Freudenberg <stefan@agaric.com>
2017-05-12 11:40:04 +0200

    Fix coding standard violations

--- sites/all/themes/custom/parliamentwatch/template.php
+++ sites/all/themes/custom/parliamentwatch/template.php
@@ -41,15 +41,15 @@ function parliamentwatch_form_comment_form_alter(&$form, &$form_state) {
  * Removes unnecessary core css files.
  */
 function parliamentwatch_css_alter(&$css) {
-  unset($css[drupal_get_path('module','system').'/system.base.css']);
-  unset($css[drupal_get_path('module','system').'/system.menus.css']);
-  unset($css[drupal_get_path('module','system').'/system.theme.css']);
-  unset($css[drupal_get_path('module','system').'/system.messages.css']);
-  unset($css[drupal_get_path('module','comment').'/comment.css']);
-  unset($css[drupal_get_path('module','search').'/search.css']);
-  unset($css[drupal_get_path('module','node').'/node.css']);
-  unset($css[drupal_get_path('module','field').'/theme/field.css']);
-  unset($css[drupal_get_path('module','user').'/user.css']);
+  unset($css[drupal_get_path('module','system') . '/system.base.css']);
+  unset($css[drupal_get_path('module','system') . '/system.menus.css']);
+  unset($css[drupal_get_path('module','system') . '/system.theme.css']);
+  unset($css[drupal_get_path('module','system') . '/system.messages.css']);
+  unset($css[drupal_get_path('module','comment') . '/comment.css']);
+  unset($css[drupal_get_path('module','search') . '/search.css']);
+  unset($css[drupal_get_path('module','node') . '/node.css']);
+  unset($css[drupal_get_path('module','field') . '/theme/field.css']);
+  unset($css[drupal_get_path('module','user') . '/user.css']);
 }
 
 /**
