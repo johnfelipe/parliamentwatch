cb8e5c3065bf74ed519a4e2506c9163671df0fed
Stefan Freudenberg <stefan@agaric.com>
2017-05-22 20:49:06 +0200

    Implement update hook for deleting obsolete fields

--- provisioning/database.sh
+++ provisioning/database.sh
@@ -12,6 +12,7 @@ echo "This will take a few minutes. Please be patient."
 echo "Downloading latest backup from http://backups.parliamentwatch.org/abgeordnetenwatch.de/abgeordnetenwatch.de_latest.sql.gz..."
 wget --http-user=$AW_USERNAME --http-password=$AW_PASSWORD --progress=bar:force -qO- http://backups.parliamentwatch.org/abgeordnetenwatch.de/abgeordnetenwatch.de_latest.sql.gz | gunzip > /tmp/backup.sql
 echo "Loading data..."
+drush sql-drop -y -p
 drush sql-cli -p < /tmp/backup.sql
 echo "Running update hooks..."
 drush updb -y -p
