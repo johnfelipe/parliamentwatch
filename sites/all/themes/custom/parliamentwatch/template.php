3b8c4c65116332b432d405bfb0154535c73d54e2
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2017-05-29 22:24:58 +0200

    Exclude css-file of core filter-module

--- sites/all/themes/custom/parliamentwatch/template.php
+++ sites/all/themes/custom/parliamentwatch/template.php
@@ -44,6 +44,7 @@ function parliamentwatch_css_alter(&$css) {
   unset($css[drupal_get_path('module','system') . '/system.menus.css']);
   unset($css[drupal_get_path('module','system') . '/system.theme.css']);
   unset($css[drupal_get_path('module','system') . '/system.messages.css']);
+  unset($css[drupal_get_path('module','filter') . '/filter.css']);
   unset($css[drupal_get_path('module','comment') . '/comment.css']);
   unset($css[drupal_get_path('module','search') . '/search.css']);
   unset($css[drupal_get_path('module','node') . '/node.css']);
