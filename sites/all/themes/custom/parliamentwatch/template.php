af62d5fc25676b673f9e8b87cf9ae078fa510273
Stefan Freudenberg <stefan@agaric.com>
2017-05-29 12:24:59 +0200

    Override theme_item_list()

--- sites/all/themes/custom/parliamentwatch/template.php
+++ sites/all/themes/custom/parliamentwatch/template.php
@@ -672,3 +672,48 @@ function _parliamentwatch_form_set_class(array &$element, array $name) {
     $element['#attributes']['class'][] = 'form__item__control--invalid';
   }
 }
+
+/**
+ *
+ * @return type
+ */
+function parliamentwatch_item_list(&$variables) {
+  $items = $variables['items'];
+  $type = $variables['type'];
+  $attributes = $variables['attributes'];
+
+  if (!empty($items)) {
+    $output .= "<$type" . drupal_attributes($attributes) . '>';
+    $num_items = count($items);
+    $i = 0;
+    foreach ($items as $item) {
+      $attributes = array();
+      $children = array();
+      $data = '';
+      $i++;
+      if (is_array($item)) {
+        foreach ($item as $key => $value) {
+          if ($key == 'data') {
+            $data = $value;
+          }
+          elseif ($key == 'children') {
+            $children = $value;
+          }
+          else {
+            $attributes[$key] = $value;
+          }
+        }
+      }
+      else {
+        $data = $item;
+      }
+      if (count($children) > 0) {
+        // Render nested list.
+        $data .= theme_item_list(array('items' => $children, 'title' => NULL, 'type' => $type, 'attributes' => $attributes));
+      }
+      $output .= '<li' . drupal_attributes($attributes) . '>' . $data . "</li>\n";
+    }
+    $output .= "</$type>";
+  }
+  return $output;
+}
