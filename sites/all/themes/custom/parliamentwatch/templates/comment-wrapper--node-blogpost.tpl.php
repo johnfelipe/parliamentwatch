249d39a6c9d529b70ef7d556bae5b83a60498d01
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2017-05-29 22:23:51 +0200

    AW-4019 Add comment specific styling and templates

--- sites/all/themes/custom/parliamentwatch/templates/comment-wrapper--node-blogpost.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/comment-wrapper--node-blogpost.tpl.php
@@ -43,12 +43,10 @@
       <h2 class="title"><?php print t('Comments'); ?></h2>
       <?php print render($title_suffix); ?>
     <?php endif; ?>
-
     <?php print render($content['comments']); ?>
-
-    <?php if ($content['comment_form']): ?>
-      <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
-      <?php print render($content['comment_form']); ?>
-    <?php endif; ?>
   </div>
+  <?php if ($content['comment_form']): ?>
+    <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
+    <?php print render($content['comment_form']); ?>
+  <?php endif; ?>
 </div>
