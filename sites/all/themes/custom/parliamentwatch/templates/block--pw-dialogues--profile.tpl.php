13de46520be8de2585bed90a04756d9d97400b3a
Stefan Freudenberg <stefan@agaric.com>
2017-06-28 13:02:49 +0200

    Restructure markup of dialogues profile tab
    Because the question form should always be displayed it must be part of
    the block content. This requires changes to the CSS.

--- sites/all/themes/custom/parliamentwatch/templates/block--pw-dialogues--profile.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/block--pw-dialogues--profile.tpl.php
@@ -45,95 +45,12 @@
  */
 ?>
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> tabs__content--qa">
-  <div class="container">
-    <div class="tabs__content__title option-title">
-      <h2><a href="#<?php print $block_html_id ?>"><?php print $block->subject; ?></a></h2>
-      <?php print render($title_suffix) ?>
-      <a href="#" class="btn btn--white btn--mobile-block">Alle Fragen in der Übersicht</a>
-    </div>
-    <div class="tabs__content__content">
-      <div class="swiper-container swiper-container--tile">
-        <div class="swiper-wrapper">
-          <?php print $content; ?>
-        </div>
-        <div class="swiper-button-prev"></div>
-        <a href="#" class="btn btn--large-white">Alle Fragen in der Übersicht</a>
-        <div class="swiper-button-next"></div>
-      </div>
-      <div class="tab-qa-stats">
-        <div class="row">
-          <div class="tab-qa-stats__col">
-            <div class="hstats__item hstats__item--donut-digit">
-              <div class="hstats__item__display mh-item-nr" data-mh="qa_hstats">
-                <span class="d3 d3--gauge"
-                    data-d3-gauge
-                    data-percentage="<?php print $block->answer_ratio ?>"
-                    data-track-width="16"
-                    data-track-colour="f6997a"
-                    data-fill-colour="ffffff"
-                    data-text-colour="ffffff"
-                    data-stroke-colour="f68a66"
-                    data-stroke-spacing="2">
-                  <span>%</span>
-                </span>
-              </div>
-              <div class="hstats__item__label mh-item-nr" data-mh="qa_hstats">
-                <?php print t('@answers of total<br>@questions questions answered', ['@answers' => $block->answers, '@questions' => $block->questions]); ?>
-              </div>
-            </div>
-            <div class="hstats__item hstats__item--digit">
-              <span class="hstats__item__display mh-item-nr" data-mh="qa_hstats">5</span>
-              <span class="hstats__item__label mh-item-nr" data-mh="qa_hstats"><?php print t('Questioner per question') ?></span>
-            </div>
-          </div>
-          <div class="tab-qa-stats__col">
-            <div class="hstats__item hstats__item--digit">
-              <span class="hstats__item__display mh-item-nr" data-mh="qa_hstats"><?php print $block->average_response_time; ?></span>
-              <span class="hstats__item__label mh-item-nr" data-mh="qa_hstats"><?php print t('average time to answer') ?></span>
-            </div>
-          </div>
-          <div class="tab-qa-stats__col--large">
-            <strong><?php print t('Questions by category') ?></strong>
-            <span class='d3 d3--bars-vertical'
-                data-d3-bars-vert
-                data-data='parseDialogues'
-                data-height='100'
-                data-fill-colour='ffffff'
-                data-stroke-colour='f68a66'>
-              <span class="tooltip d3__tooltip"></span>
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="tabs__content__title option-title">
+    <h2><a href="#<?php print $block_html_id ?>"><?php print $block->subject; ?></a></h2>
+    <?php print render($title_suffix) ?>
+    <a href="#" class="btn btn--white btn--mobile-block">Alle Fragen in der Übersicht</a>
   </div>
   <div class="tabs__content__content">
-    <div class="container">
-      <div class="row">
-        <div class="question-form">
-          <h2><?php print t('Ask question') ?></h2>
-          <?php
-            print render($block->question_form['body']);
-            print render($block->question_form['webform']);
-          ?>
-        </div>
-        <div class="question-side">
-          <div class="question-side__item">
-            <div class="info-element info-element--icon">
-              <h3><?php print t('Moderation') ?></h3>
-              <p>
-                <?php print t('The unblocking of questions can vary depending on the user\'s volume. Some hours, as all incoming questions are checked by a moderator team. I have read the moderation code and ensured that my question does not violate this. <a href="">Call moderation code</a> If my question can not be released, I will be informed by a moderator. For reasons of legal certainty, your IP address is stored, but not published or passed on to third parties. Further information is available in our Privacy Policy.') ?>
-              </p>
-            </div>
-            <div class="info-element info-element--icon">
-              <h3><?php print t('Data protection') ?></h3>
-              <p>
-                <?php print t('Data protection - Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.') ?>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php print $content; ?>
   </div>
 </section>
