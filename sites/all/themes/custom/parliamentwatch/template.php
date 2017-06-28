d5c92055f12a9ce2edf17ae472eb47e04beaeee5
Stefan Freudenberg <stefan@agaric.com>
2017-06-28 10:58:32 +0200

    Ensure poll results have values for all categories

--- sites/all/themes/custom/parliamentwatch/template.php
+++ sites/all/themes/custom/parliamentwatch/template.php
@@ -259,13 +259,13 @@ function parliamentwatch_preprocess_node(&$variables) {
 
   if ($variables['type'] == 'poll' && isset($node->result)) {
     $variables['result'] = [
-      ['name' => 'Ja', 'color' => '#9fd773', 'count' => (int) $node->result['yes']],
-      ['name' => 'Nein', 'color' => '#cc6c5b', 'count' => (int) $node->result['no']],
-      ['name' => 'Enthalten', 'color' => '#e2e2e2', 'count' => (int) $node->result['abstain']],
-      ['name' => 'Nicht abgestimmt', 'color' => '#a6a6a6', 'count' => (int) $node->result['no-show']],
+      ['name' => 'Ja', 'color' => '#9fd773', 'count' => $node->result['yes']],
+      ['name' => 'Nein', 'color' => '#cc6c5b', 'count' => $node->result['no']],
+      ['name' => 'Enthalten', 'color' => '#e2e2e2', 'count' => $node->result['abstain']],
+      ['name' => 'Nicht abgestimmt', 'color' => '#a6a6a6', 'count' => $node->result['no-show']],
     ];
-    $variables['yays'] = (int) $node->result['yes'];
-    $variables['nays'] = (int) $node->result['no'];
+    $variables['yays'] = $node->result['yes'];
+    $variables['nays'] = $node->result['no'];
   }
 }
 
