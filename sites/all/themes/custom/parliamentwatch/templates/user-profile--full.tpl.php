0ce8de139cad5550f17faf62255078fc51dc856d
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2017-07-03 13:41:22 +0200

    AW-3975 Remove obsolete container-element

--- sites/all/themes/custom/parliamentwatch/templates/user-profile--full.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/user-profile--full.tpl.php
@@ -57,112 +57,110 @@
     }
 </script>
 <article class="deputy detail"<?php print $attributes; ?>>
-  <div class="container">
-    <div class="deputy__intro">
-      <div class="deputy__intro__sidebar">
-        <header>
-          <h1 class="deputy__title"><?php print render($user_profile['field_user_fname']); ?> <?php print render($user_profile['field_user_lname']); ?></h1>
-          <div class="deputy__subtitle">
-            <?php print render($user_profile['field_user_party']); ?> | <?php print $role; ?> <?php print render($user_profile['field_user_parliament']); ?>
-          </div>
-          <?php if (!empty($user_profile['revisions'])) { print render($user_profile['revisions']); } ?>
-        </header>
-        <figure>
-          <div class="deputy__image"><?php print render($user_profile['field_user_picture']); ?></div>
-          <a href="#" class="btn btn--block">Frage stellen</a>
-          <figcaption>
-            <?php print render($user_profile['field_user_picture_copyright']); ?>
-          </figcaption>
-        </figure>
-      </div>
-      <div class="deputy__intro__content">
-        <div class="hstats">
-          <a href="#fragen-und-antworten" class="hstats__item hstats__item--donut-digit" data-localScroll>
-            <div class="hstats__item__display mh-item-nr" data-mh="hstats">
-              <span class="d3 d3--gauge"
-                    data-d3-gauge
-                    data-percentage="<?php print $answer_ratio ?>"
-                    data-track-width="10"
-                    data-track-colour="ccc"
-                    data-fill-colour="f46b3b"
-                    data-text-colour="444"
-                    data-stroke-colour="FFFFFF"
-                    data-stroke-spacing="2">
-                <span><?php print $answer_ratio; ?>%</span>
-              </span>
-            </div>
-            <div class="hstats__item__label mh-item-nr" data-mh="hstats">
-              <div class="hstats__item__label__value">
-                <?php print $answers; ?>&thinsp;/&thinsp;<?php print $questions; ?>
-              </div>
-              <?php print format_plural($questions, t('Answered question'), t('Answered questions')) ?>
-              <i class="icon icon-info" data-tooltip-content="<?php print t('tooltip-profil-answered-questions') ?>"></i>
-            </div>
-          </a>
-          <?php if (isset($user_profile['votes_total'])): ?>
-          <a href="#abstimmverhalten" class="hstats__item hstats__item--donut-digit" data-localScroll>
-            <div class="hstats__item__display mh-item-nr" data-mh="hstats">
-              <span class="d3 d3--gauge"
-                    data-d3-gauge
-                    data-percentage="<?php print $voting_ratio; ?>"
-                    data-track-width="10"
-                    data-track-colour="ccc"
-                    data-fill-colour="f46b3b"
-                    data-text-colour="444"
-                    data-stroke-colour="FFFFFF"
-                    data-stroke-spacing="2">
-                <span><?php print $voting_ratio; ?>%</span>
-              </span>
-            </div>
-            <div class="hstats__item__label mh-item-nr" data-mh="hstats">
-              <div class="hstats__item__label__value">
-                <?php print $user_profile['votes_attended']; ?>&thinsp;/&thinsp;<?php print $user_profile['votes_total']; ?>
-              </div>
-              <?php print format_plural($user_profile['votes_attended'], t('Vote by name'), t('Votes by name')) ?>
-              <i class="icon icon-info" data-tooltip-content="<?php print t('tooltip-profil-votes-by-name') ?>"></i>
-            </div>
-          </a>
-          <?php endif; ?>
+  <div class="deputy__intro">
+    <div class="deputy__intro__sidebar">
+      <header>
+        <h1 class="deputy__title"><?php print render($user_profile['field_user_fname']); ?> <?php print render($user_profile['field_user_lname']); ?></h1>
+        <div class="deputy__subtitle">
+          <?php print render($user_profile['field_user_party']); ?> | <?php print $role; ?> <?php print render($user_profile['field_user_parliament']); ?>
         </div>
-        <dl class="dl">
-          <?php if (isset($user_profile['field_user_birthday'])): ?>
-            <dt class="dl__dt"><?php print $user_profile['field_user_birthday']['#title']; ?></dt>
-            <dd class="dl__dd"><?php print $user_profile['field_user_birthday'][0]['#markup']; ?> (<?php print $user_profile['field_user_age'][0]['#markup']; ?> Jahre)</dd>
-          <?php endif; ?>
-          <?php if (isset($user_profile['field_user_childs'])): ?>
-            <dt class="dl__dt"><?php print $user_profile['field_user_childs']['#title']; ?></dt>
-            <dd class="dl__dd"><?php print $user_profile['field_user_childs'][0]['#markup']; ?></dd>
-          <?php endif; ?>
-          <?php if (isset($user_profile['field_user_marriage_status'])): ?>
-            <dt class="dl__dt"><?php print $user_profile['field_user_marriage_status']['#title']; ?></dt>
-            <dd class="dl__dd"><?php print $user_profile['field_user_marriage_status'][0]['#markup']; ?></dd>
-          <?php endif; ?>
-          <?php if (isset($user_profile['field_user_address']['#items'][0]['locality'])): ?>
-            <dt class="dl__dt"><?php print t('Wohnort') ?></dt>
-            <dd class="dl__dd"><?php print $user_profile['field_user_address']['#items'][0]['locality']; ?></dd>
-          <?php endif; ?>
-          <?php if (isset($user_profile['field_user_education'])): ?>
-            <dt class="dl__dt"><?php print $user_profile['field_user_education']['#title']; ?></dt>
-            <dd class="dl__dd"><?php print $user_profile['field_user_education'][0]['#markup']; ?></dd>
-          <?php endif; ?>
-          <?php if (isset($user_profile['field_user_list'])): ?>
-            <dt class="dl__dt"><?php print $user_profile['field_user_list']['#title']; ?></dt>
-            <dd class="dl__dd"><?php print $user_profile['field_user_list'][0]['#markup']; ?><?php if ($user_profile['field_user_list_position']): ?>, Platz <?php print $user_profile['field_user_list_position'][0]['#markup']; ?><?php endif; ?></dd>
-          <?php endif; ?>
-          <?php if (isset($user_profile['field_user_parliament'])): ?>
-            <dt class="dl__dt"><?php print $user_profile['field_user_parliament']['#title']; ?></dt>
-            <dd class="dl__dd"><?php print $user_profile['field_user_parliament'][0]['#markup']; ?></dd>
-          <?php endif; ?>
-          <?php if (isset($user_profile['field_user_election_result'])): ?>
-            <dt class="dl__dt"><?php print $user_profile['field_user_election_result']['#title']; ?></dt>
-            <dd class="dl__dd"><?php print $user_profile['field_user_election_result'][0]['#markup']; ?></dd>
-          <?php endif; ?>
-          <?php if (isset($user_profile['field_user_constituency'])): ?>
-            <dt class="dl__dt"><?php print $user_profile['field_user_constituency']['#title']; ?></dt>
-            <dd class="dl__dd"><?php print $user_profile['field_user_constituency'][0]['#markup']; ?></dd>
-          <?php endif; ?>
-        </dl>
+        <?php if (!empty($user_profile['revisions'])) { print render($user_profile['revisions']); } ?>
+      </header>
+      <figure>
+        <div class="deputy__image"><?php print render($user_profile['field_user_picture']); ?></div>
+        <a href="#" class="btn btn--block">Frage stellen</a>
+        <figcaption>
+          <?php print render($user_profile['field_user_picture_copyright']); ?>
+        </figcaption>
+      </figure>
+    </div>
+    <div class="deputy__intro__content">
+      <div class="hstats">
+        <a href="#fragen-und-antworten" class="hstats__item hstats__item--donut-digit" data-localScroll>
+          <div class="hstats__item__display mh-item-nr" data-mh="hstats">
+            <span class="d3 d3--gauge"
+                  data-d3-gauge
+                  data-percentage="<?php print $answer_ratio ?>"
+                  data-track-width="10"
+                  data-track-colour="ccc"
+                  data-fill-colour="f46b3b"
+                  data-text-colour="444"
+                  data-stroke-colour="FFFFFF"
+                  data-stroke-spacing="2">
+              <span><?php print $answer_ratio; ?>%</span>
+            </span>
+          </div>
+          <div class="hstats__item__label mh-item-nr" data-mh="hstats">
+            <div class="hstats__item__label__value">
+              <?php print $answers; ?>&thinsp;/&thinsp;<?php print $questions; ?>
+            </div>
+            <?php print format_plural($questions, t('Answered question'), t('Answered questions')) ?>
+            <i class="icon icon-info" data-tooltip-content="<?php print t('tooltip-profil-answered-questions') ?>"></i>
+          </div>
+        </a>
+        <?php if (isset($user_profile['votes_total'])): ?>
+        <a href="#abstimmverhalten" class="hstats__item hstats__item--donut-digit" data-localScroll>
+          <div class="hstats__item__display mh-item-nr" data-mh="hstats">
+            <span class="d3 d3--gauge"
+                  data-d3-gauge
+                  data-percentage="<?php print $voting_ratio; ?>"
+                  data-track-width="10"
+                  data-track-colour="ccc"
+                  data-fill-colour="f46b3b"
+                  data-text-colour="444"
+                  data-stroke-colour="FFFFFF"
+                  data-stroke-spacing="2">
+              <span><?php print $voting_ratio; ?>%</span>
+            </span>
+          </div>
+          <div class="hstats__item__label mh-item-nr" data-mh="hstats">
+            <div class="hstats__item__label__value">
+              <?php print $user_profile['votes_attended']; ?>&thinsp;/&thinsp;<?php print $user_profile['votes_total']; ?>
+            </div>
+            <?php print format_plural($user_profile['votes_attended'], t('Vote by name'), t('Votes by name')) ?>
+            <i class="icon icon-info" data-tooltip-content="<?php print t('tooltip-profil-votes-by-name') ?>"></i>
+          </div>
+        </a>
+        <?php endif; ?>
       </div>
+      <dl class="dl">
+        <?php if (isset($user_profile['field_user_birthday'])): ?>
+          <dt class="dl__dt"><?php print $user_profile['field_user_birthday']['#title']; ?></dt>
+          <dd class="dl__dd"><?php print $user_profile['field_user_birthday'][0]['#markup']; ?> (<?php print $user_profile['field_user_age'][0]['#markup']; ?> Jahre)</dd>
+        <?php endif; ?>
+        <?php if (isset($user_profile['field_user_childs'])): ?>
+          <dt class="dl__dt"><?php print $user_profile['field_user_childs']['#title']; ?></dt>
+          <dd class="dl__dd"><?php print $user_profile['field_user_childs'][0]['#markup']; ?></dd>
+        <?php endif; ?>
+        <?php if (isset($user_profile['field_user_marriage_status'])): ?>
+          <dt class="dl__dt"><?php print $user_profile['field_user_marriage_status']['#title']; ?></dt>
+          <dd class="dl__dd"><?php print $user_profile['field_user_marriage_status'][0]['#markup']; ?></dd>
+        <?php endif; ?>
+        <?php if (isset($user_profile['field_user_address']['#items'][0]['locality'])): ?>
+          <dt class="dl__dt"><?php print t('Wohnort') ?></dt>
+          <dd class="dl__dd"><?php print $user_profile['field_user_address']['#items'][0]['locality']; ?></dd>
+        <?php endif; ?>
+        <?php if (isset($user_profile['field_user_education'])): ?>
+          <dt class="dl__dt"><?php print $user_profile['field_user_education']['#title']; ?></dt>
+          <dd class="dl__dd"><?php print $user_profile['field_user_education'][0]['#markup']; ?></dd>
+        <?php endif; ?>
+        <?php if (isset($user_profile['field_user_list'])): ?>
+          <dt class="dl__dt"><?php print $user_profile['field_user_list']['#title']; ?></dt>
+          <dd class="dl__dd"><?php print $user_profile['field_user_list'][0]['#markup']; ?><?php if ($user_profile['field_user_list_position']): ?>, Platz <?php print $user_profile['field_user_list_position'][0]['#markup']; ?><?php endif; ?></dd>
+        <?php endif; ?>
+        <?php if (isset($user_profile['field_user_parliament'])): ?>
+          <dt class="dl__dt"><?php print $user_profile['field_user_parliament']['#title']; ?></dt>
+          <dd class="dl__dd"><?php print $user_profile['field_user_parliament'][0]['#markup']; ?></dd>
+        <?php endif; ?>
+        <?php if (isset($user_profile['field_user_election_result'])): ?>
+          <dt class="dl__dt"><?php print $user_profile['field_user_election_result']['#title']; ?></dt>
+          <dd class="dl__dd"><?php print $user_profile['field_user_election_result'][0]['#markup']; ?></dd>
+        <?php endif; ?>
+        <?php if (isset($user_profile['field_user_constituency'])): ?>
+          <dt class="dl__dt"><?php print $user_profile['field_user_constituency']['#title']; ?></dt>
+          <dd class="dl__dd"><?php print $user_profile['field_user_constituency'][0]['#markup']; ?></dd>
+        <?php endif; ?>
+      </dl>
     </div>
   </div>
 </article>
