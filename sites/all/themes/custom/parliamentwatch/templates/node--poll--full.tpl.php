94a4ba2fb7feda0fcf0d2a4976de221e77e93a52
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2017-07-11 20:57:04 +0200

    Correct name-spacing of sidebar-classes

--- sites/all/themes/custom/parliamentwatch/templates/node--poll--full.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/node--poll--full.tpl.php
@@ -86,11 +86,11 @@
     <div class="poll__content__left">
       <?php print render($content['body']); ?>
     </div>
-    <div class="poll__content__right">
+    <div class="poll__content__right sidebar">
       <?php if ($content['field_blogpost_categories']): ?>
-      <div class="sidebar-box">
-        <h3 class="sidebar-box__headline"><?php print t('Tags'); ?> <i class="icon icon-info" data-tooltip-content="<?php print t('tooltip-poll-tags') ?>"></i></h3>
-        <div class="sidebar-box__tag_list">
+      <div class="sidebar__box">
+        <h3 class="sidebar__box__headline"><?php print t('Tags'); ?> <i class="icon icon-info" data-tooltip-content="<?php print t('tooltip-poll-tags') ?>"></i></h3>
+        <div class="sidebar__box__tag_list">
           <?php print render($content['field_blogpost_categories']); ?>
         </div>
       </div>
