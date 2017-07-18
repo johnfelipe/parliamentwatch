057bb51ad5b71933bafad0d2c00dfaf4f775c852
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2017-07-18 14:48:32 +0200

    AW-4464 Remove blank_script.js and set content inline

--- sites/all/themes/custom/blank/js/blank_script.js
+++
@@ -1,5 +0,0 @@
-/*
- * Custom global js/jquery
- */
-
-var awpym = new pym.Child({ id: 'awpym' });
--- sites/all/themes/custom/blank/templates/html.tpl.php
+++ sites/all/themes/custom/blank/templates/html.tpl.php
@@ -18,5 +18,8 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <script language="javascript">
+      var pymChild = new pym.Child({ id: 'awpym' });
+  </script>
 </body>
 </html>
