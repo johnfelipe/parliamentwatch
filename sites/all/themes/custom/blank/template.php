47d5ec1860e6a885c1e7f8c30baf079e86a07c23
Stefan Freudenberg <stefan@agaric.com>
2017-07-28 08:31:46 +0200

    Prevent PHP warnings

--- sites/all/themes/custom/blank/template.php
+++ sites/all/themes/custom/blank/template.php
@@ -4,6 +4,6 @@
  * Implements hook_preprocess_html().
  */
 function blank_preprocess_html(&$variables) {
-  $variables['tracking'] = render(block_get_blocks_by_region('tracking'));
-  $variables['assets'] = render(block_get_blocks_by_region('assets'));
+  $variables['tracking'] = block_get_blocks_by_region('tracking');
+  $variables['assets'] = block_get_blocks_by_region('assets');
 }
