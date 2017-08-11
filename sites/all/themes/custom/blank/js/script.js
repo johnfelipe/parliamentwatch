8f940db7ce99ac003e3a0d76b85de24a61101b39
Stefan Ruh <ruh@abgeordnetenwatch.de>
2017-08-11 09:19:46 +0200

    Improve twitter/pym interaction

--- sites/all/themes/custom/blank/js/script.js
+++ sites/all/themes/custom/blank/js/script.js
@@ -1,16 +1,21 @@
 $(document).ready( function(){
-  var pymChild = new pym.Child({ id: 'awpym' });
+  var pymChild = new pym.Child({id: 'awpym'});
   var searchForm = $("form[action*='bundestag/profile']");
   if (searchForm) {
-    searchForm.submit(function(e){
+    searchForm.submit(function(event){
       pymChild.sendMessage('awSearchSubmit', searchForm.serialize());
     });
   }
-  if (typeof twttr !== 'undefined') {
-    twttr.events.bind(
-      'loaded',
-      function (event) {
-        window.dispatchEvent(new Event('resize'));
+  // account for async height change caused by twitter widget
+  if (typeof(twttr) !== 'undefined'){
+    twttr.ready(
+      function(twttr){
+        twttr.events.bind(
+          'loaded',
+          function(event){
+            pymChild.sendHeight();
+          }
+        );
       }
     );
   }
