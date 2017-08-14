d3d65bd6b40b789953754c258d0257bb4d2b3470
Stefan Freudenberg <stefan@agaric.com>
2017-08-14 16:11:44 +0200

    Prevent rendering content container on dialogue pages

--- sites/all/themes/custom/parliamentwatch/template.php
+++ sites/all/themes/custom/parliamentwatch/template.php
@@ -70,10 +70,10 @@ function parliamentwatch_page_alter(&$page) {
 
     $has_container = !empty(array_filter($page['content']['system_main']['#theme_wrappers'], $filter));
     $has_filters = isset($page['content']['system_main']['filters']);
-    $is_blog_or_petition_page = menu_get_object('node') ? in_array(menu_get_object('node')->type, ['blogpost', 'pw_petition']) : FALSE;
+    $is_blog_or_petition_or_dialogue_page = menu_get_object('node') ? in_array(menu_get_object('node')->type, ['blogpost', 'dialogue', 'pw_petition']) : FALSE;
     $is_profile_page = in_array(menu_get_item()['page_callback'], ['user_view_page', 'user_revision_show']);
 
-    if (!$has_container && !$has_filters && !$is_blog_or_petition_page && !$is_profile_page) {
+    if (!$has_container && !$has_filters && !$is_blog_or_petition_or_dialogue_page && !$is_profile_page) {
       $page['content']['system_main']['#prefix'] = '<div class="container">';
       $page['content']['system_main']['#suffix'] = '</div>';
     }
