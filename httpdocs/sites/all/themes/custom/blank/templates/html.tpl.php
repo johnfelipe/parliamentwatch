138f9fa34851e5ac94e11ae7ee19c9cad2f71283
Stefan Ruh <ruh@abgeordnetenwatch.de>
2018-03-28 15:06:02 +0200

    AW-5440 Prevent embed pages from being indexed by search engines

--- httpdocs/sites/all/themes/custom/blank/templates/html.tpl.php
+++ httpdocs/sites/all/themes/custom/blank/templates/html.tpl.php
@@ -3,6 +3,7 @@
 <head>
   <?php print $head; ?>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <meta name="robots" content="noindex" />
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
