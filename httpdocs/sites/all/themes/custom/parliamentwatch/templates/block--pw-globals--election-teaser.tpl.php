c4e6a39b311576a7a75b4130ab91829178a8986a
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2018-08-20 17:41:19 +0200

    AW-5852 Add new election-teaser as block-element

---
+++ httpdocs/sites/all/themes/custom/parliamentwatch/templates/block--pw-globals--election-teaser.tpl.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a block.
+ *
+ * Available variables:
+ * - $block->subject: Block title.
+ * - $content: Block content.
+ * - $block->module: Module that generated the block.
+ * - $block->delta: An ID for the block, unique within each module.
+ * - $block->region: The block region embedding the current block.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - block: The current template type, i.e., "theming hook".
+ *   - block-[module]: The module generating the block. For example, the user
+ *     module is responsible for handling the default user navigation block. In
+ *     that case the class would be 'block-user'.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * Helper variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $block_zebra: Outputs 'odd' and 'even' dependent on each block region.
+ * - $zebra: Same output as $block_zebra but independent of any block region.
+ * - $block_id: Counter dependent on each block region.
+ * - $id: Same output as $block_id but independent of any block region.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ * - $block_html_id: A valid HTML ID and guaranteed unique.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_block()
+ * @see template_process()
+ *
+ * @ingroup themeable
+ */
+?>
+
+<?php
+  $node_url = url('node/' . $elements['#node']->nid, ['absolute' => TRUE]);
+?>
+
+<div class="election-teaser">
+  <a class="election-teaser__col" href="/bayern">
+    <div class="election-teaser__col__title">
+      <i class="icon icon-de-by"></i>
+      <small>Landtagswahl</small>
+      <span>Bayern</span>
+    </div>
+    <span class="btn btn--small">Zum Wahlportal</span>
+  </a>
+  <a class="election-teaser__col election-teaser__col--soon" href="#">
+    <div class="election-teaser__col__title">
+      <i class="icon icon-de-he"></i>
+      <small>Landtagswahl</small>
+      <span>Hessen</span>
+    </div>
+    <span class="btn btn--small btn--disabled">In kürze verfügbar</span>
+  </a>
+</div>
