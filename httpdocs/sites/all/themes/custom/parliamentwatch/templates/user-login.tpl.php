60387aac370b2a3c1feeff4254fffa0f801d08c6
Stefan Freudenberg <stefan@agaric.com>
2018-08-27 16:57:16 +0200

    Move help text for login form to template

--- httpdocs/sites/all/themes/custom/parliamentwatch/templates/user-login.tpl.php
+++ httpdocs/sites/all/themes/custom/parliamentwatch/templates/user-login.tpl.php
@@ -1,12 +1,10 @@
 <h1><?php print t('Login'); ?></h1>
+<p><?php print t('If you are a politician you can log in and edit your profile.'); ?></p>
 <?php print drupal_render($form['form_build_id']); ?>
 <?php print drupal_render($form['form_id']); ?>
-
-<?php print drupal_render($form['help']); ?>
 <?php print drupal_render($form['name']); ?>
 <?php print drupal_render($form['pass']); ?>
 <?php print drupal_render($form['actions']); ?>
-
 <div class="form__item form__item--request-password">
   <a href="/user/password" class="link-icon"><i class="icon icon-arrow-right"></i> <?php print t('Reset password'); ?></a>
 </div>
